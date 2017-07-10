cccb3b8f518eed76fc39ce877ad95291c73a9e46
natalialuzuriaga <nat.luzuriaga@gmail.com>
2017-07-10 10:42:30 -0400

    In this commit, I added some physics features to the game

--- PeevedPenguins/GameScene.swift
+++ PeevedPenguins/GameScene.swift
@@ -7,83 +7,156 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //Vars
+    var cameraNode:SKCameraNode!
+    var buttonRestart: MSButtonNode!
+    
+    var catapult: SKSpriteNode!
+    var catapultArm: SKSpriteNode!
+    var cantileverNode: SKSpriteNode!
+    var touchNode: SKSpriteNode!
+    var penguinJoint: SKPhysicsJointPin?
+    
+    var cameraTarget: SKSpriteNode?
+
+    var touchJoint: SKPhysicsJointSpring?
     
     override func didMove(to view: SKView) {
+        /* Set reference to catapultArm node */
+        catapultArm = childNode(withName: "catapultArm") as! SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //Set reference to catapult node
+        catapult = childNode(withName: "catapult") as! SKSpriteNode
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //Set reference to cantileverNode
+        cantileverNode = childNode(withName: "cantileverNode") as! SKSpriteNode
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        //Set reference to touchNode
+        touchNode = childNode(withName:"touchNode") as! SKSpriteNode
+        
+        //Create a new Camera
+        cameraNode = childNode(withName: "cameraNode") as! SKCameraNode
+        self.camera = cameraNode
+        
+        buttonRestart = childNode(withName: "//buttonRestart") as! MSButtonNode
+        
+        buttonRestart.selectedHandler = {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            guard let scene = GameScene.level(1) else {
+                print("Level 1 is missing")
+                return
+            }
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
         }
+        setupCatapult()
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func setupCatapult() {
+        /* Pin joint */
+        var pinLocation = catapultArm.position
+        pinLocation.x += -10
+        pinLocation.y += -70
+        let catapultJoint = SKPhysicsJointPin.joint(
+            withBodyA:catapult.physicsBody!,
+            bodyB: catapultArm.physicsBody!,
+            anchor: pinLocation)
+        physicsWorld.add(catapultJoint)
+        
+        /* Spring joint catapult arm and cantilever node */
+        var anchorAPosition = catapultArm.position
+        anchorAPosition.x += 0
+        anchorAPosition.y += 50
+        let catapultSpringJoint = SKPhysicsJointSpring.joint(withBodyA: catapultArm.physicsBody!, bodyB: cantileverNode.physicsBody!, anchorA: anchorAPosition, anchorB: cantileverNode.position)
+        physicsWorld.add(catapultSpringJoint)
+        catapultSpringJoint.frequency = 6
+        catapultSpringJoint.damping = 0.5
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        /* Called when a touch begins */
+        let touch = touches.first!              // Get the first touch
+        let location = touch.location(in: self) // Find the location of that touch in this view
+        let nodeAtPoint = atPoint(location)     // Find the node at that location
+        if nodeAtPoint.name == "catapultArm" {  // If the touched node is named "catapultArm" do...
+            touchNode.position = location
+            touchJoint = SKPhysicsJointSpring.joint(withBodyA: touchNode.physicsBody!, bodyB: catapultArm.physicsBody!, anchorA: location, anchorB: location)
+            physicsWorld.add(touchJoint!)
+            
+            let penguin = Penguin()
+            addChild(penguin)
+            penguin.position.x += catapultArm.position.x + 20
+            penguin.position.y += catapultArm.position.y + 50
+            penguin.physicsBody?.usesPreciseCollisionDetection = true
+            penguinJoint = SKPhysicsJointPin.joint(withBodyA: catapultArm.physicsBody!,
+                                                   bodyB: penguin.physicsBody!,
+                                                   anchor: penguin.position)
+            physicsWorld.add(penguinJoint!)
+            cameraTarget = penguin
+            
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        touchNode.position = location
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if let touchJoint = touchJoint {
+            physicsWorld.remove(touchJoint)
+        }
+        // Check for a touchJoint then remove it.
+        if let touchJoint = touchJoint {
+            physicsWorld.remove(touchJoint)
+        }
+        // Check for a penguin joint then remove it.
+        if let penguinJoint = penguinJoint {
+            physicsWorld.remove(penguinJoint)
+        }
+        // Check if there is a penguin assigned to the cameraTarget
+        guard let penguin = cameraTarget else {
+            return
+        }
+        // Generate a vector and a force based on the angle of the arm.
+        let force: CGFloat = 350
+        let r = catapultArm.zRotation
+        let dx = cos(r) * force
+        let dy = sin(r) * force
+        // Apply an impulse at the vector. 
+        let v = CGVector(dx: dx, dy: dy)
+        penguin.physicsBody?.applyImpulse(v)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    override func update(_ currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        moveCamera()
     }
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    /* Make a Class method to load levels */
+    class func level(_ levelNumber: Int) -> GameScene? {
+        guard let scene = GameScene(fileNamed: "Level_\(levelNumber)") else {
+            return nil
+        }
+        scene.scaleMode = .aspectFill
+        return scene
     }
+    
+    func moveCamera() {
+        guard let cameraTarget = cameraTarget else {
+            return
+        }
+        let targetX = cameraTarget.position.x
+        let x = clamp(value: targetX, lower: 0, upper: 392)
+        cameraNode.position.x = x
+    }
+}
+
+func clamp<T: Comparable>(value: T, lower: T, upper: T) -> T {
+    return min(max(value, lower), upper)
 }
